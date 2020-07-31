d20f7808d815b63d1183490ca23eea3e0c68f1f2
MandarinOrange <istars0110@gmail.com>
2020-07-31 15:12:46 +0800

    js基础、函数、Window对象、BOM编程

---
+++ js/my.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/07/01.
+ */
+var a;
+a = 3;
+document.write(a+"<br>");
+
+var b = 20;//声明变量的同时给变量赋值
+document.write(b+"<br>");
+
+var c= 2,d=25.6,e=true;
+document.write(c+"-"+d+"-"+e+"<br>")
+
+f="abc";//建议不要省略var
+document.write(f);
